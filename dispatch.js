7cba35d322e5552de60ee964c7c345d1d4e677c1
lixiaobin <lixiaobin@baijiahulian.com>
2017-06-06 16:39:32 +0800

    添加证书

--- dispatch.js
+++ dispatch.js
@@ -6,5 +6,8 @@ const egg = require('egg');
 env.NODE_ENV = 'production';
 
 egg.startCluster({
-    baseDir: __dirname
+    baseDir: __dirname,
+    https: true,
+    key: './7479d459-1a34-4550-a4b9-75cd10a9e3af.key',
+    cert: './7479d459-1a34-4550-a4b9-75cd10a9e3af.pem'
 });
