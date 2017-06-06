cad18b1706c308af9426e2a215082b5843dae68a
lixiaobin <lixiaobin@baijiahulian.com>
2017-06-06 17:14:30 +0800

    https使用8443

--- dispatch.js
+++ dispatch.js
@@ -8,7 +8,6 @@ env.NODE_ENV = 'production';
 egg.startCluster({
     baseDir: __dirname,
     https: true,
-    port: 7001,
     key: './7479d459-1a34-4550-a4b9-75cd10a9e3af.key',
     cert: './7479d459-1a34-4550-a4b9-75cd10a9e3af.pem'
 });
