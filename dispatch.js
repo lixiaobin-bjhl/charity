435ac3ed6f7e47df43def90adadd2dfd301b1c2a
lixiaobin <lixiaobin@baijiahulian.com>
2017-06-07 17:31:04 +0800

    更换ssl证书

--- dispatch.js
+++ dispatch.js
@@ -9,5 +9,5 @@ egg.startCluster({
     baseDir: __dirname,
     https: true,
     key: './7479d459-1a34-4550-a4b9-75cd10a9e3af.key',
-    cert: './7479d459-1a34-4550-a4b9-75cd10a9e3af.pem'
+    cert: './server.pem'
 });
